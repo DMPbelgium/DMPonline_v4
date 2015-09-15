43a7ebc46cd4d89b3136dc8043af6649301aab6d
njfranck <njfranck@ca20c521.ugent.be>
2015-09-15 16:16:58 +0200

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,14 +11,14 @@ class ApplicationController < ActionController::Base
  	after_filter :store_location
 
 	def store_location
-	 # store last url - this is needed for post-login redirect to whatever the user last visited.
+	  # store last url - this is needed for post-login redirect to whatever the user last visited.
 		if (request.fullpath != "/users/sign_in" && \
-			request.fullpath != "/users/sign_up" && \
+		  request.fullpath != "/users/sign_up" && \
 			request.fullpath != "/users/password" && \
-            request.fullpath != "/users/sign_up?nosplash=true" && \
+      request.fullpath != "/users/sign_up?nosplash=true" && \
 			!request.xhr?) # don't store ajax calls
 		  session[:previous_url] = request.fullpath
-   		end
+    end
 	end
 
 	def after_sign_in_path_for(resource)
