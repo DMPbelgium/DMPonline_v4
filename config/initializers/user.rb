10dcf06ae401bf5b937b38ca043621515bcdc304
njfranck <njfranck@ca20c521.ugent.be>
2015-10-20 16:07:48 +0200

    only change organisation when not set

--- config/initializers/user.rb
+++ config/initializers/user.rb
@@ -1,45 +1,48 @@
 User.after_auth_shibboleth do |user,auth,request|
-  domain = request.host
 
+  #only change organisation when none exists
   if user.organisation.nil?
+
+    domain = request.host
     orgs = Organisation.where(:domain => domain)
+
     if orgs.size > 0
       user.organisation_id=(orgs.first.id)
-    end
-  end
+      if domain =~ /ugent.be$/
 
-  if domain =~ /ugent.be$/
+        user.surname = auth['extra']['raw_info']['sn']
+        user.firstname = auth['extra']['raw_info']['givenname']
 
-    user.surname = auth['extra']['raw_info']['sn']
-    user.firstname = auth['extra']['raw_info']['givenname']
+        #try to match against a more specific suborganisation
+        if !user.organisation.nil? && user.organisation.is_parent?
 
-    #try to match against a more specific suborganisation
-    if !user.organisation.nil? && user.organisation.is_parent?
+          faculty = nil
 
-      faculty = nil
+          if !auth['extra']['raw_info']['ugentdeptnumber1'].nil?
 
-      if !auth['extra']['raw_info']['ugentdeptnumber1'].nil?
+            faculty = auth['extra']['raw_info']['ugentdeptnumber1'][0,2]
 
-        faculty = auth['extra']['raw_info']['ugentdeptnumber1'][0,2]
+          elsif !auth['extra']['raw_info']['ugentfaculty'].nil?
 
-      elsif !auth['extra']['raw_info']['ugentfaculty'].nil?
+            faculty = auth['extra']['raw_info']['ugentfaculty']
 
-        faculty = auth['extra']['raw_info']['ugentfaculty']
+          end
 
-      end
+          unless faculty.nil?
 
-      unless faculty.nil?
+            fac_orgs = Organisation.where(:abbreviation => faculty, :parent_id => user.organisation.id)
+            if fac_orgs.size > 0
 
-        fac_orgs = Organisation.where(:abbreviation => faculty, :parent_id => user.organisation.id)
-        if fac_orgs.size > 0
+              user.organisation_id=(fac_orgs.first.id)
 
-          user.organisation_id=(fac_orgs.first.id)
+            end
 
+          end
         end
 
       end
-    end
 
+    end
   end
 
   user.save if user.changed?
