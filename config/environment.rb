63b2b4f01ff342e8b682227cf7e42a7112f5ff1b
Nicolas Franck <njfranck@ca20c533.ugent.be>
2017-10-20 16:46:32 +0200

    https://github.com/FineLinePrototyping/activerecord-native_db_types_override/issues/1

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 DMPonline4::Application.initialize!
+
+=begin
+  Yuck, hate to do this, but older mysql2 gems set primary key to NULL,
+  which is not acceptable for more modern mysql servers.
+=end
+class ActiveRecord::ConnectionAdapters::Mysql2Adapter
+  NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
+end
