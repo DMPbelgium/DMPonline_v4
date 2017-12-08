be4c4cb91f7e620322bb0e850c3a2c1a3fc5dff2
Nicolas Franck <njfranck@ca20c526.ugent.be>
2017-12-08 14:10:55 +0100

    possible bug fix for SystemStackError?

--- app/models/suggested_answer.rb
+++ app/models/suggested_answer.rb
@@ -1,9 +1,13 @@
 class SuggestedAnswer < ActiveRecord::Base
 
-	belongs_to :organisation, :inverse_of => :suggested_answers, :autosave => true
-	belongs_to :question, :inverse_of => :suggested_answers, :autosave => true
+	belongs_to :organisation, :inverse_of => :suggested_answers
+	belongs_to :question, :inverse_of => :suggested_answers
 
-	accepts_nested_attributes_for :question
+  #TODO: this, together with "autosave=true" for the relations "belongs_to",
+  #triggers "SystemStackError: stack level too deep"
+  #probably due to callback chain child->parent->child->parent ..
+  #Possible side effects of this "fix"?
+	#accepts_nested_attributes_for :question
 
 	attr_accessible :organisation_id, :question_id, :text, :is_example
 
